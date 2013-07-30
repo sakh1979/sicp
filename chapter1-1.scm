f103b5a69fd9d3902c48ee122d2ee7ebc7d3a120
Sagun Khatri <sakh1979@gmail.com>
2013-07-30 07:23:41 -0500

    Syntax formating for pretty viewing

--- chapter1-1.scm
+++ chapter1-1.scm
@@ -47,8 +47,8 @@
   (define (improve guess)
     (average guess (/ x guess)))
   (define (sqrt-iter guess)
-    (if (good-enough? guess)
-        guess
-        (sqrt-iter (improve guess))))
-  (sqrt-iter 1.0))
+    (if (good-enough? guess) guess
+    (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0)
+)
 
