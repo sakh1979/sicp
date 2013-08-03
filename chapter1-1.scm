328c4647f59c3480539fdd4050d80097d5fd12ee
Sagun Khatri <sakh1979@gmail.com>
2013-08-02 22:06:09 -0500

    Up to Exercise 1.2

--- chapter1-1.scm
+++ chapter1-1.scm
@@ -22,3 +22,11 @@
 ;;;          ((< a b) b)
 ;;;          (else -1))
 ;;;    (+ a 1))
+
+
+;;; Exercise 1.2
+;;; Translate the following expression into prefix form
+;;; Answer => -37/150
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
+    (* 3 (- 6 2) (- 2 7)))
+
