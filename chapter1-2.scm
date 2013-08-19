fc032181d35e2f4b80dd70f880bd9f4457877cb3
Sagun Khatri <sakh1979@gmail.com>
2013-08-19 17:30:12 -0500

    Exercise 1.11 partially completed

--- chapter1-2.scm
+++ chapter1-2.scm
@@ -192,3 +192,22 @@
 ;;;
 ;;; => (define (h n) (A 2 n)) => 2^2^...(n times)
 ;;;
+;;;
+
+
+
+;;; Exercise 1.11
+;;; A function f is defined by the rule that f(n) = n if n < 3 and
+;;; f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n >= 3. Write a procedure
+;;; that computes f by means of recursive process. Write a procedure that
+;;; computes f by means of an iterative process.
+
+;;; recursive version
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f(- n 1)) 
+                 (* 2 (f(- n 2))) 
+                 (* 3 (f(- n 3)))))))
+
+;;; iterative version
+
