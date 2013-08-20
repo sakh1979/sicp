11d29c864cc77a2f512751fd69db342e29c1d4cf
Sagun Khatri <sakh1979@gmail.com>
2013-08-20 17:52:24 -0500

    Upto Exercise 1.12

--- chapter1-2.scm
+++ chapter1-2.scm
@@ -223,3 +223,23 @@
                       b
                       (+ count 1)
                       n))))
+
+
+
+;;; Exercise 1.12
+;;; The following pattern of numbers is called Pascal's triangle.
+;;;
+;;;            1
+;;;           1 1
+;;;          1 2 1
+;;;         1 3 3 1
+;;;        1 4 6 4 1
+;;; 
+;;; The numbers at the edge of the triangle are all 1, and each number 
+;;; inside the triangle is the sum of the two numbers above it.
+;;; Write a procedure that computes elements of Pascal's triangle by 
+;;; means of a recursive process.
+
+(define (pascal c r)
+  (cond ((or (= c 0) (= c r)) 1)
+        (else (+ (pascal(- c 1) (- r 1)) (pascal c (- r 1))))))
