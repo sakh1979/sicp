fbbadec9f9ee51125ec9d3d72b9414e4941b1068
Sagun Khatri <sakh1979@gmail.com>
2013-08-22 14:49:26 -0500

    Upto Exercise 1.17

--- chapter1-2.scm
+++ chapter1-2.scm
@@ -270,3 +270,37 @@
           (else (* b (expt-iter (- n 1) a)))))
 
   (expt-iter n 1))
+
+
+
+
+;;; Exercise 1.17
+;;; The exponentiation algorithms in this section are based on performing 
+;;; exponentiation by means of repeated multiplication. In a similar way, 
+;;; one can perform integer multiplication by means of repeated addition. 
+;;; The following multiplication procedure (in which it is assumed that 
+;;; our language can only add, not multiply) is analogous to the 
+;;; expt procedure:
+;;;
+;;; (define (* a b)
+;;;   (if (= b 0) 0
+;;;       (+ a (* a (- b 1)))))
+;;;
+;;; This algorithm takes a number of steps that is linear in b. Now 
+;;; suppose we include, together with addition, operations double, 
+;;; which doubles an integer, and halve, which divides an (even) integer 
+;;; by 2. Using these, design a multiplication procedure analogous to 
+;;; fast-expt that uses a logarithmic number of steps.
+
+(define (fast-mult a b)
+
+  (define double (lambda (x) (+ x x)))
+
+  (define halve (lambda (x) (/ x 2)))
+
+  (define (mult-iter n sum)
+    (cond ((= n 0) sum)
+          ((even? n) (double (mult-iter (halve n) sum)))
+          (else (+ a (mult-iter (- n 1) sum)))))
+
+  (mult-iter b 0))
