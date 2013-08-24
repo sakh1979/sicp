8dc05561fddc83f18e30de59a4e8e0a945c49ec0
Sagun Khatri <sakh1979@gmail.com>
2013-08-24 14:52:31 -0500

    Exercise 1.19 partial commit

--- chapter1-2.scm
+++ chapter1-2.scm
@@ -318,3 +318,43 @@
           (else (+ a (mult-iter (- n 1) sum)))))
 
   (mult-iter b 0))
+
+
+
+;;; Exercise 1.19
+;;; There is a clever algorithm for computing the Fibonacci numbers in a 
+;;; logarithmic number of steps. Recall the transformation of the 
+;;; state variables a and b in the fib-iter process of section 1.2.2:
+;;; a = a + b and b = a. Call this transformation T, and observe that 
+;;; applying T over and over again n times, starting with 1 and 0, 
+;;; produces the pair Fib(n + 1) and Fib(n). In other words, the Fibonacci 
+;;; numbers are produced by applying T^n, the nth power of the 
+;;; transformation T, starting with the pair (1,0). Now consider T to be 
+;;; the special case of p = 0 and q = 1 in a family of transformations
+;;; Tpq, where Tpq transforms the pair(a, b) according to a = bq + aq + ap
+;;; and b = bp + aq. Show that if we apply such transformation Tp`q` twice, 
+;;; the effect is the same as using a single transformation Tpq for the
+;;; same form, and compute p` and q` in terms of p and q. This gives us an
+;;; explicit way to square these transformation, and thus we and compute
+;;; T^n using successive squaring, as in the fast-expt procedure. Put this
+;;; all together to complete the following procedure, which runs in a 
+;;; logarithmic nuber of steps.
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define square (lambda (x) (* x x)))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (square (+ a b)) ; compute p`
+                   (square a)       ; compute q`
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
