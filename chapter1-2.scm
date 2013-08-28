55a6b8df206909900f2137c33e266e65d464d60e
Sagun Khatri <sakh1979@gmail.com>
2013-08-28 18:07:02 -0500

    Upto Exercise 1.22

--- chapter1-2.scm
+++ chapter1-2.scm
@@ -1,3 +1,11 @@
+;;; Sagun Khatri
+;;; August 28th, 2013
+;;; Utilities procedues
+
+(load "utils")
+
+
+
 ;;; Exercise 1.9
 ;;; Each of the following two procedures defines a method for adding two
 ;;; positive integers in terms of the procedures inc, which increments
@@ -262,8 +270,6 @@
 
 (define (fast-expt b n)
 
-  (define square (lambda (x) (* x x)))
-
   (define (expt-iter n a)
     (cond ((= n 0) a)
           ((even? n) (square (expt-iter (/ n 2) a)))
@@ -343,8 +349,6 @@
 (define (fib n)
   (fib-iter 1 0 0 1 n))
 
-(define square (lambda (x) (* x x)))
-
 (define (fib-iter a b p q count)
   (cond ((= count 0) b)
         ((even? count)
@@ -377,3 +381,74 @@
           (else (find-divisor (+ test-divisor 1)))))
 
   (find-divisor 2))
+
+
+
+
+;;; Exercise 1.22
+;;; Most Lisp implementations include a primitive called runtime that 
+;;; returns an integer that specifies the amount of time the system 
+;;; has been running (measured, for example, in microseconds). The 
+;;; following timed-prime-test procedure, when called with an integer n, 
+;;; prints n and checks to see if n is prime. If n is prime, the procedure 
+;;; prints three asterisks followed by the amount of time used in 
+;;; performing the test.
+;;;
+;;; (define (timed-prime-test n)
+;;;   (newline)
+;;;   (display n)
+;;;   (start-prime-test n (runtime)))
+;;;
+;;; (define (start-prime-test n start-time)
+;;;   (if (prime? n) (report-prime (- (runtime) start-time))))
+;;;
+;;; (define (report-prime elapsed-time)
+;;;   (display " *** ")
+;;;   (display elasped-time))
+;;;
+;;;
+;;; Using this procedure, write a procedure search-for-primes that checks 
+;;; the primality of consecutive odd integers in a specified range. Use 
+;;; your procedure to find the three smallest primes larger than 1000; 
+;;; larger than 10,000; larger than 100,000; larger than 1,000,000. 
+;;; Note the time needed to test each prime. Since the testing algorithm 
+;;; has order of growth of O(sqrt(n)), you should expect that testing for 
+;;; primes around 10,000 should take about (sqrt 10) times as long as testing
+;;; for pimes around 1000. Do your timing data bear this out? How well do the
+;;; data for 100,000 and 1,000,000 support the (sqrt n) prediction? Is your
+;;; result compatible with the notion that programs on your machine run in 
+;;; time proportional to the number of steps required for the computation?
+
+(define (timed-prime-test n)
+  (display n)
+  (start-prime-test n (runtime))
+  (newline))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 5) (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (newline)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (search-for-primes value counter)
+  (cond ((>= counter 3) (display "DONE!!!") (newline))
+        ((prime? value) (timed-prime-test value) 
+                        (search-for-primes (+ value 2) (+ counter 1))) 
+        (else (search-for-primes (+ value 2) counter))))
+
