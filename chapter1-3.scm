f87dc50d069e71c3c5eaadfdb9d2b0f9f4d9fa84
Sagun Khatri <sakh1979@gmail.com>
2013-09-03 20:05:58 -0500

    Upto Exercise 1.36

--- chapter1-3.scm
+++ chapter1-3.scm
@@ -98,3 +98,24 @@
 
 (define golden-ratio 
   (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+
+
+
+
+;;; ---------------------------------------------------------------------
+;;; Exercise 1.36
+
+(define (fixed-point-modify f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define (x-to-the-x y)
+  (fixed-point-modify (lambda (x) (/ (log y) (log x))) 10.0))
