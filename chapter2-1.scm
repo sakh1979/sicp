f96dd919bd3fd167129c97610858abf5e7563af1
Sagun Khatri <sakh1979@gmail.com>
2013-09-11 17:33:29 -0500

    Upto Exercise 2.6

--- chapter2-1.scm
+++ chapter2-1.scm
@@ -111,3 +111,41 @@
 
 (define (cdr z)
   (z (lambda (p q) q)))
+
+
+
+
+;;; ------------------------------------------------------------------------
+;;; Exercise 2.6
+
+(define zero (lambda (f) (lambda (x) x)))
+
+; => Use substitution to evaluate (add-1 zero)
+; ; => (add-1 zero)
+; ; => (lambda (f) (lambda (x) (f ((zero f) x))))
+; ; => (lambda (f) (lambda (x) (f x)))
+; ; => Which is the definition of Church Numerals One!
+(define one (lambda (f) (lambda (x) (f x))))
+
+; => Using substitution method to evaluate (add-1 one)
+; ; => (add-1 one)
+; ; => (lambda (f) (lambda (x) (f ((one f) x))))
+; ; => (lambda (f) (lambda (x) (f (f x))))
+; ; => Which is the definition of Church Numerals Two!
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define (int->church n)
+  (cond ((= n 0) zero)
+        (else (add-1 (int->church (- n 1))))))
+
+(define (church->int f)
+  ((f (lambda (x) (+ x 1))) 0))
+
+(define (add a b)
+  (lambda (f) 
+    (lambda (x)
+      ((a f) ((b f) x)))))
+
