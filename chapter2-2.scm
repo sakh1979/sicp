39d895024a9c92cdf5d8114936b22d16cc15c7a0
Sagun Khatri <sakh1979@gmail.com>
2013-09-17 16:11:12 -0500

    Upto Exercise 2.19

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -21,3 +21,26 @@
     (cond ((null? input) output)
           (else (helper (cdr input) (cons (car input) output)))))
   (helper alist (list )))
+
+
+
+;;; Exercise 2.19
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomination coin-values))
+                 coin-values)))))
+
+(define (no-more? alist)
+  (null? alist))
+
+(define (except-first-denomination alist)
+  (cdr alist))
+
+(define (first-denomination alist)
+  (car alist))
