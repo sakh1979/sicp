bd590df8920cdbacaa1e3bd35fef86294795c841
Sagun Khatri <sakh1979@gmail.com>
2013-09-17 15:54:42 -0500

    Upto Exercise 2.18

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -11,3 +11,13 @@
     (cond ((= current-counter (- len 1)) alist)
           (else (iter (cdr alist) (+ current-counter 1) len))))
   (iter alist 0 (length alist)))
+
+
+
+;;; Exercise 2.18
+
+(define (reverse alist)
+  (define (helper input output)
+    (cond ((null? input) output)
+          (else (helper (cdr input) (cons (car input) output)))))
+  (helper alist (list )))
