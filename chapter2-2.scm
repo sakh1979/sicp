50b906e979acae5a3efe6fecd1ed58d3d7e7a62e
Sagun Khatri <sakh1979@gmail.com>
2013-09-19 16:44:55 -0500

    Upto Exercise 2.26

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -127,3 +127,15 @@
 ;;;
 ;;; (1 (2 (3 (4 (5 (6 7))))))   => 
 ;;; (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr x))))))))))))
+
+
+
+
+;;; Exercise 2.26
+;;;
+;;; (define x (list 1 2 3))
+;;; (define x (list 1 2 3))
+;;;
+;;; (append x y) => (1 2 3 4 5 6)
+;;; (cons x y)   => ((1 2 3) 4 5 6)
+;;; (list x y)   => ((1 2 3) (4 5 6))
