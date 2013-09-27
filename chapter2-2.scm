71a5af9f28ed35f04c95b39465da2dd7e6627d71
Sagun Khatri <sakh1979@gmail.com>
2013-09-26 19:00:37 -0500

    Upto Exercise 2.31

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -237,3 +237,17 @@
          (cond ((pair? sub-tree) (square-tree sub-tree))
                (else (square sub-tree))))
        tree))
+
+
+
+
+;;; Exercise 2.31
+
+(define (tree-map f tree)
+  (map (lambda (sub-tree)
+         (cond ((pair? sub-tree) (tree-map f sub-tree))
+               (else (f sub-tree))))
+       tree))
+
+(define (square-tree tree) (tree-map square tree))
+
