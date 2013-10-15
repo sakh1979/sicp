201147a4efa3b6a9ce4a2edd2e1ebce57768bdef
Sagun Khatri <sakh1979@gmail.com>
2013-10-14 19:15:15 -0500

    Upto Exercise 2.38

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -316,3 +316,19 @@
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
     (map (lambda (rows) (matrix-*-vector cols rows)) m)))
+
+
+;;; Exercise 2.38
+
+(define (fold-left operation initial sequence)
+  (define (iter result rest)
+    (if (null? rest) result
+        (iter (operation result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+; Answer:
+; (fold-right / 1 (list 1 2 3)) => 3/2
+; (fold-left / 1 (list 1 2 3)) => 1/6
+; (fold-right list nil (list 1 2 3)) => (1 (2 (3 ())))
+; (fold-left list nil (list 1 2 3)) => (((() 1) 2) 3)
