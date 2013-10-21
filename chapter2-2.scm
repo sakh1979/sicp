72ece74df89a06660f461755d3a5eb566f90600f
Sagun Khatri <sakh1979@gmail.com>
2013-10-21 18:39:37 -0500

    Upto Exercise 2.40

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -341,3 +341,27 @@
 
 (define (fold-left-reverse sequence)
   (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+
+;;; Exercise 2.40
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
+
+(define (unique-pairs n)
+  (flatmap
+   (lambda (i)
+     (map (lambda (j) (list i j))
+          (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 1 n)))
