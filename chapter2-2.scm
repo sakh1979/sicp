7a285982e51e73444069ecd46642e0e378ea4e20
Sagun Khatri <sakh1979@gmail.com>
2013-10-25 19:42:59 -0500

    Exercise 2.42

--- chapter2-2.scm
+++ chapter2-2.scm
@@ -388,3 +388,52 @@
   (map make-triple-sum
        (filter triple-sum?
                (ordered-triples n))))
+
+
+;;; Exercise 2.42
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define (make-position row col)
+  (cons row col))
+
+(define (position-row position)
+  (car position))
+
+(define (position-col position)
+  (cdr position))
+
+(define empty-board nil)
+
+(define (adjoin-position row col positions)
+  (append positions (list (make-position row col))))
+
+(define (safe? col positions)
+  (let ((kth-queen (list-ref positions (- col 1)))
+        (other-queens (filter (lambda (q)
+                                (not (= col (position-col q))))
+                              positions)))
+
+    (define (attacks? q1 q2)
+      (or (= (position-row q1) (position-row q2))
+          (= (abs (- (position-row q1) (position-row q2)))
+             (abs (- (position-col q1) (position-col q2))))))
+
+    (define (iter q board)
+      (or (null? board)
+          (and (not (attacks? q (car board)))
+               (iter q (cdr board)))))
+
+    (iter kth-queen other-queens)))
